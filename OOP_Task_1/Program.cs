a615ed38dc71203c937a8ed4ed0be46b28f22020
m00nler <91968598+m00nler@users.noreply.github.com>
2021-12-21 21:52:43 +0200

    Added file moving

--- OOP_Task_1/Program.cs
+++ OOP_Task_1/Program.cs
@@ -6,7 +6,10 @@ namespace OOP_Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            File_Info fileInfo = new File_Info(@"C:\Users\moonler\Desktop\мусор");
+            fileInfo.File_Output();
+            fileInfo.Enter_File_Name("1, Ричко.doc");
+            fileInfo.Folder_Move(@"C:\Users\moonler\Desktop");
         }
     }
 }
