21ea08e75ae953559872679bc01929926df9f235
m00nler <91968598+m00nler@users.noreply.github.com>
2021-12-22 17:23:59 +0200

    Added some functions

--- OOP_Task_1/File_Info.cs
+++ OOP_Task_1/File_Info.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 
 namespace OOP_Task_1
@@ -36,6 +38,62 @@ namespace OOP_Task_1
                 .ForEach(f => Console.WriteLine(Path.GetFileName(f)));
         }
 
+        public void File_Output_By_Name()
+        {
+            Directory
+                .GetFiles(_Path, "*", SearchOption.AllDirectories)
+                .ToList()
+                .OrderBy(f=>f)
+                .ToList()
+                .ForEach(f => Console.WriteLine(Path.GetFileName(f)));
+        }
+
+        public void File_Output_By_Size()
+        {
+            List<string> lst= Directory.GetFiles(_Path).ToList();
+            lst
+                .OrderBy(f => new FileInfo(f).Length)
+                .ToList()
+                .ForEach(f=>Console.WriteLine(Path.GetFileName(f)));
+        }
+
+        public void File_Output_Hidden_Files_Too()
+        {
+            string[] lst_info = Directory.GetFiles(_Path);
+            FileAttributes[] arr=new FileAttributes[lst_info.ToList().Count];
+            int ind = 0;
+            foreach (var i in lst_info)
+            {
+                arr[ind] = File.GetAttributes(i);
+                ind++;
+            }
+
+            ind = 0;
+            List<FileAttributes> lst = arr.ToList();
+            foreach (var i in lst)
+            {
+                if((i & FileAttributes.Hidden)==FileAttributes.Hidden)
+                {
+                    arr[ind] = RemoveAttribute(i, FileAttributes.Hidden);
+                    File.SetAttributes(lst_info[ind],arr[ind]);
+                }
+            }
+            File_Output();
+        }
+        private FileAttributes RemoveAttribute(FileAttributes attributes, FileAttributes attributesToRemove)
+        {
+            return attributes & ~attributesToRemove;
+        }
+
+        public void File_Output_By_Extension()
+        {
+            List<string> lst = Directory.GetFiles(_Path).ToList();
+            lst
+                .OrderBy(f=>Path.GetExtension(f))
+                .ToList()
+                .ForEach(f=>Console.WriteLine(Path.GetFileName(f)));
+        }
+        
         public void Change_Folder(string path)
         {
             _Path = path;
@@ -59,16 +117,38 @@ namespace OOP_Task_1
 
         public void Delete_Folder(string folder_name)
         {
-            
-            // void DeleteDirectory(string directoryName, bool checkDirectiryExist)
-            // {
-            //     if (Directory.Exists(directoryName))
-            //         Directory.Delete(directoryName, true);
-            //     else if (checkDirectiryExist)
-            //         throw new SystemException("Directory you want to delete is not exist");
-            // }
-            
             Directory.Delete($@"{_Path}\{folder_name}",true);
         }
+
+        public void Create_File(string file_name)
+        {
+            var file=File.Create($@"{_Path}\{file_name}");
+            file.Close();
+        }
+
+        public void Delete_File(string file_name)
+        {
+            File.Delete($@"{_Path}\{file_name}");
+        }
+
+        public void Rename_File(string file_name, string new_file_name)
+        {
+            string source_file = $@"{_Path}\{file_name}";
+            FileInfo fi = new FileInfo(source_file);
+            if (fi.Exists)
+            {
+                fi.MoveTo($@"{_Path}\{new_file_name}");
+            }
+        }
+
+        public void Rename_Folder(string folder_name, string new_folder_name)
+        {
+            DirectoryInfo di = new DirectoryInfo($@"{_Path}\{folder_name}");
+            if (di.Exists == true)
+            {
+                Directory.Move($@"{_Path}\{folder_name}",$@"{_Path}\{new_folder_name}");
+            }
+        }
+        
     }
 }
--- OOP_Task_1/Program.cs
+++ OOP_Task_1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace OOP_Task_1
 {
@@ -7,14 +6,19 @@ namespace OOP_Task_1
     {
         static void Main(string[] args)
         {
-            File_Info file = new File_Info(@"C:\Users\moonler\Desktop\мусор");
-            //file.File_Output();
-            file.Enter_File_Name("1, Ричко.doc");
+            File_Info file = new File_Info(@"E:\Projects\Test_Rep_1");
+            //file.File_Output_By_Extension();
+            //file.Enter_File_Name("1, Ричко.doc");
             //file.Folder_Move(@"C:\Users\moonler\Desktop");
             file.Change_Folder(@"C:\Users\moonler\Desktop\мусор");
+            file.File_Output_Hidden_Files_Too();
             file.Create_Folder("ВАЛЕРЧИК_ТЕСТ");
             Console.WriteLine(file.Current_Path());
-            file.Delete_Folder("фывфыв");
+            //file.Delete_Folder("фывфыв");
+            file.Create_File("Валерчик тест.txt");
+            file.Delete_File("Валерчик тест.txt");
+            file.Rename_File("5c3c9a1ca93c0.jpeg","test.jpeg");
+            //file.Rename_Folder("ВАЛЕРЧИК_ТЕСТ","тест_ренейм");
         }
     }
 }
