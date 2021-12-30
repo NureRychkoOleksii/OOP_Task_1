3664fc020efb6c9ccb9191fe61e24fa6199a85ef
m00nler <91968598+m00nler@users.noreply.github.com>
2021-12-30 13:44:34 +0200

    Changed unit-tests

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System.Xml.IXmlBinaryReaderInitializer;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
